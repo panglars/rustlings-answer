796c026c5f205a91ce309303d8b48db0d250b8dc
PangLAN <i@pll.moe>
2024-08-14 10:04:21 +0800

    finish to test4

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -7,10 +7,11 @@
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
+}
+pub fn is_odd(num: i32) -> bool {
+    num % 2 == 1
 }
 
 #[cfg(test)]
@@ -19,11 +20,11 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(!is_even(5));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        assert!(is_odd(5));
     }
 }
